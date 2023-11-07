b4ef9f94e98737ad96d9d6989b41f4739a9a508c
J M <jin.ma@zstack.io>
2023-11-07 16:25:07 +0800

    <fix>[imagestorebackupstorage]: support nvme model
    Resolves: SUG-622
    
    Change-Id: I6f68696f776c7974716d7476697262677361636e

--- imagestorebackupstorage/ansible/imagestorebackupstorage.py
+++ imagestorebackupstorage/ansible/imagestorebackupstorage.py
@@ -105,7 +105,7 @@ else :
 zstacklib = ZstackLib(zstacklib_args)
 
 if host_info.distro in RPM_BASED_OS:
-    qemu_pkg = "fuse-sshfs nmap collectd tar net-tools blktrace"
+    qemu_pkg = "fuse-sshfs nmap collectd tar net-tools blktrace nvme-cli"
 
     releasever_mapping = {
         'h84r': ' collectd-disk pyparted',
@@ -239,6 +239,18 @@ if client == "false" and new_add == "false":
     dst_dir = os.path.join(fs_rootpath, "registry")
     if not file_dir_exist("path=" + dst_dir, host_post_info):
         error("ERROR: registry directory is missing, imagestore metadata may have been lost. Check it immediately!")
+
+def load_nvme():
+    command = "modinfo nvme_fabrics"
+    status = run_remote_command(command, host_post_info, True, False)
+    if status is False:
+        return "nvme-fabics kernel module not found!"
+    command = "/sbin/modprobe nvme nvme_core nvme_fabrics"
+    status = run_remote_command(command, host_post_info, True, False)
+    if status is False:
+        return "failed to load nvme kernel module"
+
+load_nvme()
 
 # name: install zstack-store
 if client == "false":
