5a800870e47fa944aaf86b3cba1c118f05b27644
shenjin <jin.shen@zstack.io>
2024-01-02 17:33:59 +0800

    <fix>[baremetalpxeserver]: update default and add new path for grub.cfg
    update uefi grub.cfg default path, and add a new boot
    path for uefi grub.cfg
    
    Resolves: ZSTAC-62422
    
    Change-Id: I6c68787a706c78736a6d6c706d687369716c7a79

--- baremetalpxeserver/baremetalpxeserver/pxeserveragent.py
+++ baremetalpxeserver/baremetalpxeserver/pxeserveragent.py
@@ -104,7 +104,8 @@ class PxeServerAgent(object):
     PXELINUX_CFG_PATH = TFTPBOOT_PATH + "pxelinux.cfg/"
     PXELINUX_DEFAULT_CFG = PXELINUX_CFG_PATH + "default"
     UEFI_GRUB_CFG_PATH = TFTPBOOT_PATH + "EFI/BOOT/"
-    UEFI_DEFAULT_GRUB_CFG = TFTPBOOT_PATH + "grub.cfg"
+    UEFI_DEFAULT_GRUB_CFG = UEFI_GRUB_CFG_PATH + "grub.cfg"
+    UEFI_BOOT_GRUB_CFG = TFTPBOOT_PATH + "grub.cfg"
     # we use `KS_CFG_PATH` to hold kickstart/preseed/autoyast preconfiguration files
     KS_CFG_PATH = VSFTPD_ROOT_PATH + "ks/"
     INSPECTOR_KS_CFG = KS_CFG_PATH + "inspector_ks_ARCH.cfg"
@@ -311,6 +312,8 @@ menuentry 'ZStack Get Bare Metal Chassis Hardware Info' --class fedora --class g
 }
 """ % (pxeserver_dhcp_nic_ip, pxeserver_dhcp_nic_ip)
         with open(self.UEFI_DEFAULT_GRUB_CFG, 'w') as f:
+            f.write(grub_cfg)
+        with open(self.UEFI_BOOT_GRUB_CFG, 'w') as f:
             f.write(grub_cfg)
 
         # init inspector_ks.cfg
