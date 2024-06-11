7a9868868e7b1e4807bee580f865063fce288fdd
shan.wu <shan.wu@zstack.io>
2024-06-11 11:27:18 +0800

    auto acquire orphan lock on sanlock
    (cherry picked from commit 66360a256f9844b1ba5a322029d6a5da7d8ee2b5)

--- kvmagent/kvmagent/plugins/shared_block_plugin.py
+++ kvmagent/kvmagent/plugins/shared_block_plugin.py
@@ -658,6 +658,7 @@ class SharedBlockPlugin(kvmagent.KvmAgent):
             lvm.modify_sanlock_config("use_zstack_vglock_timeout", 0)
             lvm.modify_sanlock_config("zstack_vglock_large_delay", 8)
             lvm.modify_sanlock_config("use_zstack_vglock_large_delay", 0)
+            lvm.modify_sanlock_config("acquire_orphan", 1)
 
             sanlock_hostname = "%s-%s-%s" % (cmd.vgUuid[:8], cmd.hostUuid[:8], linux.get_hostname()[:20])
             lvm.modify_sanlock_config("our_host_name", sanlock_hostname)
