cb78f23af1ca8b6f42bb5a4ab97ddd758db230ca
J M <jin.ma@zstack.io>
2024-09-10 17:09:20 +0800

    <fix>[zstacklib]: fix qemu 2.9 version get
    Resolves: ZSTAC-55377
    
    Change-Id: I7865656b616d677067676e736875626e656f6263

--- zstacklib/zstacklib/utils/qemu.py
+++ zstacklib/zstacklib/utils/qemu.py
@@ -113,9 +113,10 @@ def _parse_version(version_output):
 
     return "-".join(filter(lambda s: s[0].isdigit(), full_ver.split("-")))
 
+# QEMU emulator version 2.9.0(qemu-kvm-ev-2.9.0-16.el7_4.14.1)
 # QEMU emulator version 2.12.0 (qemu-kvm-ev-2.12.0-44.1.el7_9.1)
 # QEMU emulator version 4.2.0 (qemu-kvm-4.2.0-640.g70d8f25.el7)
 # return 2.12.0
 def _parse_version2(version_output):
-    return version_output.splitlines()[0].strip().split(" ")[3]
+    return version_output.splitlines()[0].strip().split(" ")[3].split("(")[0]
 
