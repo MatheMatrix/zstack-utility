0ba59a87a54d6518149305f50ff12884dad4d563
yingzhe.hu <yingzhe.hu@zstack.io>
2024-11-04 11:46:42 +0800

    <fix>[physical_memory_monitor]: physical memory monitor improved
    Resolves: ZSTAC-68712
    
    Change-Id: I7165756f74787379686377716d6264686b61677a

--- kvmagent/kvmagent/plugins/physical_memory_monitor.py
+++ kvmagent/kvmagent/plugins/physical_memory_monitor.py
@@ -3,8 +3,9 @@ from zstacklib.utils import http
 from zstacklib.utils import jsonobject
 from zstacklib.utils import lock
 from zstacklib.utils import log
-from zstacklib.utils import thread
 from zstacklib.utils import bash
+import time
+import threading
 
 log.configure_log('/var/log/zstack/zstack-kvmagent.log')
 logger = log.get_logger(__name__)
@@ -24,15 +25,12 @@ class PhysicalMemoryECCErrorAlarm(object):
 
 class PhysicalMemoryMonitor(kvmagent.KvmAgent):
 	PHYSICAL_MEMORY_MONITOR = "/host/physical/memory/monitor/start"
-	
-	state = None
-	time_lock = 0
 	interval = 60
-	trigger_flag = None
 	
 	def __init__(self):
 		self.state = False
 		self.trigger_flag = False
+		self.monitor_thread = None
 	
 	def configure(self, config):
 		self.config = config
@@ -46,34 +44,41 @@ class PhysicalMemoryMonitor(kvmagent.KvmAgent):
 	
 	@kvmagent.replyerror
 	def start_physical_memory_monitor(self, req):
-		self.time_lock += 1
-		self.state = True
 		logger.debug("start monitor physical memory!")
-		self.monitor_physical_memory_ecc_error(self.time_lock)
+		self.monitor_physical_memory_ecc_error()
 		return jsonobject.dumps(AgentRsp())
-	
+
 	@lock.lock('monitor_physical_memory_ecc_error')
-	def monitor_physical_memory_ecc_error(self, time_lock_now):
-		if time_lock_now != self.time_lock or not self.state:
-			return
-		
-		'''
-			title: monitor physical memory ECC_ERROR, include UE&&CE
-			cmd: edac-util --report=default
-			zero error return: "edac-util: No errors to report."
-			non-zero error return: "csrow0: ch0: 43722040 Corrected Errors"
-		'''
-		r, o, e = bash.bash_roe("edac-util --report=default")
-		# ZHCI-1484: The edac module will not be loaded in some env and command returns 'No memory controller data found',
-		# add a judgment to work around it.
-		# ZHCI-1502: 'No memory controller data found' may be output in std_out or std_error.
-		if r == 0 and not self.trigger_flag and ("No errors to report" not in o and "No memory controller data found" not in (o + (e if e else ""))):
-			self.send_physical_memory_ecc_error_alarm_to_mn(o)
-			self.trigger_flag = True
-		else:
-			self.trigger_flag = False
-			
-		thread.timer(self.interval, self.monitor_physical_memory_ecc_error, args=[time_lock_now]).start()
+	def monitor_physical_memory_ecc_error(self):
+		def _monitor_error():
+			while True:
+				if not self.state:
+					break
+
+				'''
+				title: monitor physical memory ECC_ERROR, include UE&&CE
+				cmd: edac-util --report=default
+				zero error return: "edac-util: No errors to report."
+				non-zero error return: "csrow0: ch0: 43722040 Corrected Errors"
+				'''
+				_, o, e = bash.bash_roe("edac-util --report=default", ret_code=-1)
+				# ZHCI-1484: The edac module will not be loaded in some env and command returns 'No memory controller data found',
+				# add a judgment to work around it.
+				# ZHCI-1502: 'No memory controller data found' may be output in std_out or std_error.
+				if "No memory controller data found" in str(o) + str(e) or "No errors to report" in str(o) + str(e):
+					self.trigger_flag = False
+				elif not self.trigger_flag:
+					self.send_physical_memory_ecc_error_alarm_to_mn(o)
+					self.trigger_flag = True
+				time.sleep(self.interval)
+
+		if self.monitor_thread and self.monitor_thread.is_alive():
+			self.state = False
+			self.monitor_thread.join()
+
+		self.state = True
+		self.monitor_thread = threading.Thread(target=_monitor_error)
+		self.monitor_thread.start()
 	
 	def send_physical_memory_ecc_error_alarm_to_mn(self, detail):
 		physical_memory_ecc_error_alarm = PhysicalMemoryECCErrorAlarm()
