1d18536e3fc3e13b571b16752d1cbd191e3235c6
J M <jin.ma@zstack.io>
2024-11-18 16:31:56 +0800

    <fix>[nfs]: rebase migrated image only once
    because fast clone, not all images in dest folder are migrated.
    
    Resolves: ZSTAC-60300
    Resolves: ZSV-9205
    
    Change-Id: I787a796a63636b677471767a7976616c6b747772
    
    (cherry picked from commit ed0e5d347f40606811c9cf645f346a5ee873978a)

--- kvmagent/kvmagent/test/utils/volume_utils.py
+++ kvmagent/kvmagent/test/utils/volume_utils.py
@@ -1,5 +1,6 @@
 import array
 import os
+import fcntl
 
 from zstacklib.test.utils import env
 from zstacklib.utils import uuidhelper, linux, xmlobject
@@ -79,6 +80,21 @@ start_vm_sharedblock_data_vol = {
         "controllerIndex": 0,
         "ioThreadId": 0
     }
+
+
+def qcow2_write_lock(path):
+    fd = os.open(path, os.O_RDWR)
+    file_lock(fd, 100, 10, fcntl.LOCK_EX)
+    return fd
+
+
+def qcow2_unlock(fd):
+    file_lock(fd, 100, 10, fcntl.LOCK_UN)
+    return fd
+
+def file_lock(fd, offset, length, flag):
+    fcntl.lockf(fd, flag, length, offset, os.SEEK_SET)
+    return fd
 
 
 def create_empty_volume(size=134217728):  # 128M
