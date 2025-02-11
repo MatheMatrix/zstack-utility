fda54c847c9b831919fa2d7f639dce0eb7404484
J M <jin.ma@zstack.io>
2025-02-11 19:06:01 +0800

    <fix>[zstacklib]: isdigit is not ishex
    Resolves: ZSTAC-71966
    
    Change-Id: I616b6d717a646469677665626e6968617a61687a

--- zstacklib/zstacklib/utils/qemu.py
+++ zstacklib/zstacklib/utils/qemu.py
@@ -5,6 +5,7 @@ import os
 import log
 import jsonobject
 import shell
+import string
 import json
 import zlib
 from zstacklib.utils import qemu_img
@@ -195,7 +196,8 @@ def read_image_content(image_path, offset, length, format):
 
     content = ""
     for line in o.splitlines():
-        if line.split(":", 1)[0].strip().isdigit():
+        h = line.split(":", 1)[0].strip()
+        if set(h).issubset(string.hexdigits):
             # encode hex str to ascii
             content += "".join(chr(int(x, 16)) for x in line.split()[1:17])
 
