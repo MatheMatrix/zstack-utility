108ea4fb30bf27d02479a8d3a523d00d41df6e32
xinhao.huang <xinhao.huang@zstack.io>
2025-04-27 16:32:57 +0800

    <feature>[host]: support enflame s60 gcu monitor
    Resolves: ZSTAC-74788
    
    Change-Id: I6b70786b6c7567677878736c7275736969616662

--- zstacklib/zstacklib/utils/pci.py
+++ zstacklib/zstacklib/utils/pci.py
@@ -7,6 +7,17 @@ import xml.etree.ElementTree as ET
 logger = log.get_logger(__name__)
 
 
+class VendorEnum:
+    INTEL = "Intel"
+    AMD = "AMD"
+    NVIDIA = "NVIDIA"
+    HAIGUANG = "Haiguang"
+    HUAWEI = "Huawei"
+    TIANSHU = "TianShu"
+    VASTAI = "Vastai"
+    ENFLAME = "Enflame"
+
+
 def is_gpu(type):
     return type in ['GPU_3D_Controller', 'GPU_Video_Controller', 'GPU_Processing_Accelerators', 'GPU_Co_Processor']
 
