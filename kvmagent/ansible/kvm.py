7e3139a9202e8953e1769fd7a71255ea9d9e8301
Zhang Wenhao <wenhao.zhang@zstack.io>
2025-04-28 10:25:13 +0800

    <fix>[kvmagent]: install Arcconf in kylin x86 host
    for test!!!!!!!!!!!!!
    
    Resolves: ZSV-8606
    
    Change-Id: I6b68636a687871796778707774657a64786d7461

--- kvmagent/ansible/kvm.py
+++ kvmagent/ansible/kvm.py
@@ -226,6 +226,12 @@ def install_kvm_pkg():
             'nfs4': 'vconfig iscsi-initiator-utils OpenIPMI nettle libselinux-devel iptables iptables-services qemu-kvm python2-pyudev collectd-disk'
         }
 
+        addition_rpms = {
+            'kylin': {
+                'x86_64': 'Arcconf'
+            }
+        }
+
         edk2_mapping = {
             'x86_64': 'edk2-ovmf edk2.git-ovmf-x64',
             'aarch64': 'edk2-aarch64'
@@ -238,6 +244,7 @@ def install_kvm_pkg():
                 os_base_dep,
                 distro_mapping.get(distro_head, ''),
                 releasever_mapping.get(releasever, ''),
+                # addition_rpms.get(releasever, {}).get(host_info.host_arch, ''),
                 edk2_mapping.get(host_info.host_arch, ''))
             # common kvmagent deps of x86 and arm that need to update
             common_update_list = ("sanlock sysfsutils hwdata sg3_utils lvm2"
