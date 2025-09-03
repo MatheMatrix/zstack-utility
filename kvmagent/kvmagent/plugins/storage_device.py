564a29a8fe3df68d02d1622246e130b78d9c3b90
shan.wu <shan.wu@zstack.io>
2025-09-03 15:45:22 +0800

    <fix>[sharedblock]: fix iscsid.conf to be cleared
    use secure write instead of `sed - i`
    
    Resolves/Related: ZSTAC-63164
    
    Change-Id: I6e716579686b6c636a73616b67637a6a747a6572
    (cherry picked from commit dca2340a56c8afc94ba865ac52facb32c287aec0)

--- kvmagent/kvmagent/plugins/storage_device.py
+++ kvmagent/kvmagent/plugins/storage_device.py
@@ -23,6 +23,7 @@ from zstacklib.utils import misc
 
 logger = log.get_logger(__name__)
 ISCSI_LOGIN_DEFAULT_TIMEOUT = 180
+ISCSID_CONFIG_PATH = "/etc/iscsi/iscsid.conf"
 
 
 class RetryException(Exception):
@@ -431,7 +432,10 @@ class StorageDevicePlugin(kvmagent.KvmAgent):
                                      "it may recover after a while so check and login again" %((len(disks_by_iscsi) - len(disks_by_no_mapping_lun)), len(disks_by_dev), disks_by_dev))
 
         def check_iscsi_conf():
-            shell.call("sed -i 's/.*iscsid.startup.*=.*/iscsid.startup = \/bin\/systemctl start iscsid.socket iscsiuio.soccket/' /etc/iscsi/iscsid.conf", exception=False)
+            if os.path.exists(ISCSID_CONFIG_PATH):
+                with linux.CrashSafeFileEditor(ISCSID_CONFIG_PATH) as content:
+                    content.text, _ = re.subn(r"^.*iscsid\.startup.*=.*$", "iscsid.startup = /bin/systemctl start iscsid.socket iscsiuio.socket",
+                                              content.text, flags=re.MULTILINE)
 
         check_iscsi_conf()
         path = "/var/lib/iscsi/nodes"
