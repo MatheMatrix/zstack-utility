b7bf478d48158f20334047d4aca43fb5ab476337
yaohua.wu <yaohua.wu@zstack.io>
2025-09-05 14:20:18 +0800

    <fix>[zstacklib]: optimize max addressable memory size calculate
    Resolves: ZSTAC-77791
    
    Change-Id: I69786f6166656c6f6d7870796669797967617575

--- zstacklib/zstacklib/utils/pci.py
+++ zstacklib/zstacklib/utils/pci.py
@@ -117,11 +117,8 @@ def calculate_max_addressable_memory(pci_devices):
         mem_size_32bit, mem_size_64bit = get_total_addressable_memory(get_pci_resources(dev.pciDeviceAddress))
         logger.info("get pci device: %s, name: %s, max addressable memory: %s" %
                     (dev.pciDeviceAddress, dev.name, mem_size_64bit))
-        max32bit += mem_size_32bit
-        max64bit += mem_size_64bit
-
-    max32bit = calc_next_power_of_2(max32bit)
-    max64bit = calc_next_power_of_2(max64bit)
+        max32bit += calc_next_power_of_2(mem_size_32bit)
+        max64bit += calc_next_power_of_2(mem_size_64bit)
 
     max_addressable_memory_32bit = max32bit * 2
     max_addressable_memory_64bit = max64bit
