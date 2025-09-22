07a2ca35518225d2b4ca43bc1d0478dc69ad600b
tao.gan <tao.gan@zstack.io>
2025-09-22 10:09:57 +0800

    <fix>[host]: <description
    Resolves: ZSV-9928
    
    Change-Id: I757972756576786c6669797a766a766464706571

---
+++ zstacklib/zstacklib/utils/file_downloader.py
@@ -0,0 +1,111 @@
+import os
+import traceback
+import urlparse
+
+from kvmagent.kvmagent import logger
+from zstacklib.utils import traceable_shell, linux, shell, plugin
+
+
+class FileDownloader:
+    URL_HTTP = 'http'
+    URL_HTTPS = 'https'
+    URL_FILE = 'file'
+    URL_SFTP = 'sftp'
+    URL_FTP = 'ftp'
+    URL_NFS = 'nfs'
+
+    def __init__(self, reporter, cmd):
+        self.reporter = reporter
+        self.cmd = cmd
+        self.t_shell = traceable_shell.get_shell(cmd)
+        self.supported_schemes = [
+            self.URL_HTTP, self.URL_HTTPS,
+            self.URL_FTP, self.URL_SFTP, self.URL_FILE
+        ]
+        self.path = os.path.dirname(self.cmd.installPath)
+        self.file_name = os.path.basename(self.cmd.installPath)
+        self.install_path = self.cmd.installPath
+        self.timeout = self.cmd.timeout if self.cmd.timeout else 7200
+
+    def percentage_callback(self, percent):
+        self.reporter.progress_report(int(percent))
+
+    def use_wget(self, url, name, workdir, timeout):
+        return linux.wget(url, workdir=workdir, rename=name, timeout=timeout, interval=2,
+                          callback=self.percentage_callback, callback_data=url)
+
+    def download_via_http(self):
+        try:
+            self.cmd.url = linux.shellquote(self.cmd.url)
+            ret = self.use_wget(self.cmd.url, self.file_name, self.path, self.timeout)
+            if ret != 0:
+                linux.rm_file_force(self.cmd.install_path)
+                return False, 'http/https/ftp download failed, [wget -O %s %s] returns value %s' % (
+                    self.file_name, self.cmd.url, ret)
+            return True, None
+        except linux.LinuxError as e:
+            linux.rm_file_force(self.install_path)
+            traceback.format_exc()
+            return False, str(e)
+
+    def download_via_sftp(self):
+        ssh_pass_file = None
+        port = (self.cmd.url.port, 22)[self.cmd.url.port is None]
+        install_path = self.cmd.installPath
+
+        class SftpDownloadDaemon(plugin.TaskDaemon):
+            def _cancel(self):
+                pass
+
+            def _get_percent(self):
+                return os.stat(install_path).st_size / (total_size / 100) if os.path.exists(install_path) else 0
+
+            def __exit__(self, exc_type, exc_val, exc_tb):
+                super(SftpDownloadDaemon, self).__exit__(exc_type, exc_val, exc_tb)
+                if ssh_pass_file:
+                    linux.rm_file_force(ssh_pass_file)
+                if exc_val is not None:
+                    linux.rm_file_force(install_path)
+                    traceback.format_exc()
+
+        try:
+            with SftpDownloadDaemon(self.cmd, "DownloadImage"):
+                sftp_cmd = "sftp -P %d -o BatchMode=no -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -b /dev/stdin %s@%s " \
+                           "<<EOF\n%%s\nEOF\n" % (port, self.cmd.url.username, self.cmd.url.hostname)
+                if self.cmd.url.password is not None:
+                    ssh_pass_file = linux.write_to_temp_file(self.cmd.url.password)
+                    sftp_cmd = 'sshpass -f %s %s' % (ssh_pass_file, sftp_cmd)
+
+                total_size = int(shell.call(sftp_cmd % ("ls -l " + self.cmd.url.path)).splitlines()[1].split()[4])
+                self.t_shell.call(sftp_cmd % ("reget %s %s" % (self.cmd.url.path, self.cmd.install_path)))
+            return True, None
+        except Exception:
+            return False, "SFTP download failed"
+
+    def download_via_file(self):
+        src_path = self.cmd.url.lstrip('file:')
+        src_path = os.path.normpath(src_path)
+        if not os.path.isfile(src_path):
+            raise Exception('cannot find the file[{src_path}]')
+
+        logger.debug("src_path is: %s" % src_path)
+        try:
+            self.t_shell.call('yes | cp %s %s' % (src_path, linux.shellquote(self.install_path)))
+            return True, None
+        except shell.ShellError as e:
+            linux.rm_file_force(self.install_path)
+            return False, str(e)
+
+    def download(self):
+        if self.cmd.urlScheme not in self.supported_schemes:
+            return False, 'unsupported url scheme[%s], only supports %s' % (self.cmd.urlScheme, self.supported_schemes)
+
+        if not os.path.exists(self.path):
+            os.makedirs(self.path, 0777)
+
+        if self.cmd.urlScheme in [self.URL_HTTP, self.URL_HTTPS, self.URL_FTP]:
+            return self.download_via_http()
+        elif self.cmd.urlScheme == self.URL_SFTP:
+            return self.download_via_sftp()
+        elif self.cmd.urlScheme == self.URL_FILE:
+            return self.download_via_file()
---
+++ zstacklib/zstacklib/utils/upload_task.py
@@ -0,0 +1,315 @@
+# encoding: utf-8
+
+import re
+import hashlib
+import logging
+from zstacklib.utils import linux, lock
+import cherrypy
+from cherrypy._cpreqbody import Entity, Part, SizedReader
+from cherrypy._cprequest import Request
+
+logger = logging.getLogger(__name__)
+BUFFER_SIZE = 16 * 1024 ** 2  # 16MB
+
+
+class UploadParam(object):
+    def __init__(self):
+        self.image_uuid = None
+        self.image_size = 0
+
+        self.slice_index = 0
+        self.slice_offset = 0
+        self.slice_size = 0
+        self.slice_md5 = None
+
+
+class UploadTasks(object):
+    MAX_RECORDS = 80
+
+    def __init__(self, lock_name):
+        self.tasks = {}
+        self.lock_name = lock_name
+
+    def add_task(self, task):
+        with lock.lock(self.lock_name):
+            if len(self.tasks) >= self.MAX_RECORDS:
+                self._expunge_oldest_task()
+            self.tasks[task.resource_uuid] = task
+
+    def get_task(self, resource_uuid):
+        with lock.lock(self.lock_name):
+            return self.tasks.get(resource_uuid)
+
+    def _expunge_oldest_task(self):
+        oldest_uuid, oldest_time = '', linux.get_current_timestamp()
+        for uuid, task in self.tasks.iteritems():
+            if task.last_op_time < oldest_time:
+                oldest_uuid, oldest_time = uuid, task.last_op_time
+
+        if oldest_uuid != '':
+            del self.tasks[oldest_uuid]
+            logger.info("Expunged oldest task: %s", oldest_uuid)
+
+
+class UploadTask(object):
+    def __init__(self, imageUuid, dstPath):
+        self.completed = False
+        self.imageUuid = imageUuid
+        self.dstPath = dstPath
+        self.expectedSize = 0
+        self.downloadSize = 0
+        self.progress = 0
+        self.lastError = None
+        self.lastOpTime = linux.get_current_timestamp()
+        self.close = None
+
+        self.slice_uploaded = set()
+        self.slice_count = 0
+        self.slice_size = 0
+
+        self.image_created = False
+        self.image_completing = False
+
+    def fail(self, reason):
+        self.completed = True
+        self.lastError = reason
+        self.lastOpTime = linux.get_current_timestamp()
+        if self.close:
+            self.close()
+        logger.info('task failed for %s: %s' % (self.imageUuid, reason))
+
+    def success(self):
+        self.completed = True
+        self.progress = 100
+        self.lastOpTime = linux.get_current_timestamp()
+        if self.close:
+            self.close()
+
+    def is_started(self):
+        return self.progress > 0
+
+    def is_running(self):
+        return not (self.completed or self.is_started())
+
+    def renew(self):
+        self.lastOpTime = linux.get_current_timestamp()
+
+    def all_slice_uploaded(self):
+        return 0 < self.slice_count == len(self.slice_uploaded)
+
+    def checked_download_size(self):
+        for i in range(self.slice_count):
+            if i not in self.slice_uploaded:
+                return i * self.slice_size
+
+        return self.expectedSize
+
+    def allow_image_completing(self):
+        if self.all_slice_uploaded():
+            with lock.NamedLock("upload-image-%s" % self.imageUuid):
+                if not self.image_completing:
+                    self.image_completing = True
+                    return True
+        return False
+
+    def add_download_size(self, delta):
+        with lock.NamedLock("upload-image-%s" % self.imageUuid):
+            self.downloadSize += delta
+
+    def create_image_if_not_exists(self, install_path):
+        raise NotImplementedError()
+
+    def complete_upload(self):
+        raise NotImplementedError()
+
+    def create_object(self, slice_offset):
+        raise NotImplementedError()
+
+
+class StorageObject(object):
+    def seek(self, offset):
+        raise NotImplementedError()
+
+    def read(self, n):
+        raise NotImplementedError()
+
+    def write(self, content):
+        raise NotImplementedError()
+
+    def close(self):
+        raise NotImplementedError()
+
+
+class UploadHandler(object):
+    def __init__(self, req, upload_tasks):
+        self.req = req
+        self.upload_tasks = upload_tasks
+
+    @staticmethod
+    def get_boundary(entity):
+        """获取multipart边界"""
+        if 'boundary' in entity.content_type.params:
+            ib = entity.content_type.params['boundary'].strip('"')
+        else:
+            return None
+
+        if not re.match("^[ -~]{0,200}[!-~]$", ib):
+            raise ValueError('Invalid boundary in multipart form: %r' % (ib,))
+
+        ib = ('--' + ib).encode('ascii')
+
+        # Find the first marker
+        while True:
+            b = entity.readline()
+            if not b:
+                return None
+
+            b = b.strip()
+            if b == ib:
+                break
+
+        return ib
+
+    @staticmethod
+    def get_upload_param(headers):
+        """解析上传参数"""
+
+        def get_long_field(key, default=None):
+            v = headers.get(key, default)
+            try:
+                lv = long(v)
+                if lv < 0:
+                    raise ValueError
+                return lv
+            except ValueError:
+                raise Exception('invalid header "%s": %s' % (key, v))
+
+        up = UploadParam()
+        up.resource_uuid = headers['X-IMAGE-UUID']
+
+        # 通用上传参数
+        up.total_size = get_long_field('X-TOTAL-SIZE')
+        up.slice_offset = get_long_field('X-SLICE-OFFSET', default=0)
+        up.slice_size = get_long_field('X-SLICE-SIZE', default=up.total_size)
+        up.slice_index = get_long_field('X-SLICE-INDEX', default=0)
+        up.slice_md5 = headers.get('X-SLICE-MD5', None)
+
+        # 参数验证
+        if up.slice_offset >= up.total_size:
+            raise Exception('invalid slice offset header: %s, total_size: %d' %
+                            (up.slice_offset, up.total_size))
+
+        return up
+
+    def get_upload_task(self, param):
+        task = self.upload_tasks.get_task(param.image_uuid)
+        if task is None:
+            raise Exception('image not found %s' % param.image_uuid)
+
+        if task.lastError:
+            self._fail_task(task, task.lastError)
+
+        if task.completed:
+            raise Exception('image[uuid: %s] upload has completed' % param.image_uuid)
+
+        task.expectedSize = param.image_size
+
+        if param.slice_index == 0:
+            task.slice_size = param.slice_size
+
+            err = task.backend.check_capacity(task.expectedSize)
+            if err:
+                self._fail_task(task, err)
+
+        if param.slice_offset + param.slice_size == param.image_size:
+            slice_count = param.slice_index + 1
+        else:
+            slice_count = (param.image_size - 1) / param.slice_size + 1
+
+        if not task.slice_count:
+            task.slice_count = slice_count
+        elif task.slice_count != slice_count:
+            raise Exception(
+                "every upload request for image[uuid:%s] should has the same slice size and image size" % param.image_uuid)
+
+        return task
+
+    @staticmethod
+    def _fail_task(task, reason):
+        task.fail(reason)
+        raise Exception(reason)
+
+    def upload_slice(self, entity, param, task):
+        boundary = self.get_boundary(entity)
+        if not boundary:
+            self._fail_task(task, 'unexpected post form')
+
+        try:
+            self.stream_body(entity, boundary, param, task)
+        except Exception, e:
+            self._fail_task(task, str(e))
+
+    @staticmethod
+    def stream_body(entity, boundary, param, task):
+        # type: (Entity, str, UploadParam, UploadTask) -> None
+
+        image_obj = task.create_object(param.slice_offset)
+        while True:
+            headers = Part.read_headers(entity.fp)
+            p = Part(entity.fp, headers, boundary)
+            if not p.filename:
+                continue
+
+            logger.debug("uploading image %s: %s slice, index: %d, offset: %d, content length: %d" %
+                         (param.image_uuid, p.filename, param.slice_index, param.slice_offset, param.slice_size))
+
+            slice_downloaded_size = 0
+            try:
+                reader = SizedReader(p.fp, None, param.slice_offset)
+                remaining = param.slice_size
+                bytes_read = 0
+                md5 = hashlib.md5()
+                chunks = []
+                chunk_size = 32 * 1024
+                while remaining > 0:
+                    if task.lastError:
+                        raise Exception(task.lastError)
+                    tmp = reader.read(min(chunk_size, remaining))
+                    datalen = len(tmp)
+                    task.renew()
+                    chunks.append(tmp)
+                    md5.update(tmp)
+
+                    remaining -= datalen
+                    bytes_read += datalen
+                    if bytes_read >= BUFFER_SIZE or remaining <= 0:
+                        image_obj.write(b''.join(chunks))
+                        task.add_download_size(bytes_read)
+                        slice_downloaded_size += bytes_read
+                        chunks = []
+                        bytes_read = 0
+            finally:
+                image_obj.close()
+            break
+
+        if param.slice_size != slice_downloaded_size:
+            task.add_download_size(-slice_downloaded_size)
+            raise Exception("incomplete image %s slice index %d, offset %d, completed %d, expected %d" %
+                            (param.image_uuid, param.slice_index, param.slice_offset, slice_downloaded_size,
+                             param.slice_size))
+
+        if param.slice_md5 and param.slice_md5 != md5.hexdigest():
+            task.add_download_size(-slice_downloaded_size)
+            raise cherrypy.HTTPError(406, "content md5 not match, expected: %s, actual: %s" % (
+                param.slice_md5, md5.hexdigest()))
+
+        task.slice_uploaded.add(param.slice_index)
+        logger.debug("uploaded image %s slice, index: %d offset: %d, content length: %d" %
+                     (param.image_uuid, param.slice_index, param.slice_offset, param.slice_size))
+
+    def handle_upload(self):
+        upload_param = self.get_upload_param(self.req.headers)
+        task = self.get_upload_task(upload_param)
+        self.upload_slice(self.req.body, upload_param, task)
+        if task.allow_image_completing():
+            task.complete_upload()
