1761c35f71c447b1bfd0644fa053e428b4686656
haoyu.ding <haoyu.ding@zstack.io>
2025-10-09 10:51:40 +0800

    <fix>[mevoco]: remove duplicated ebtable rule for dhcpv6
    Resolves: ZSTAC-76938
    
    Change-Id: I68736168736f7a627a6d61696f616c6c6e617863

--- kvmagent/kvmagent/plugins/mevoco.py
+++ kvmagent/kvmagent/plugins/mevoco.py
@@ -235,17 +235,24 @@ class DhcpEnv(object):
                 bash_errorout(
                     '%s -t mangle -A POSTROUTING -p udp -m udp --dport 68 -j CHECKSUM --checksum-fill' % IPTABLES_CMD)
 
-        def _add_ebtables_rule6(rule):
-            ret = bash_r(
-                EBTABLES_CMD + " -L {{CHAIN_NAME}} | grep -- '{{rule}}' > /dev/null")
+        def _add_ebtables_rule6(rule_search, rule_add=None):
+            if rule_add is None:
+                rule_add = rule_search
+
+            search_cmd_no_mask = EBTABLES_CMD + " -L {{CHAIN_NAME}} | grep -- '{{rule_search}}' > /dev/null"
+            ret = bash_r(search_cmd_no_mask)
+
             if ret != 0:
-                bash_errorout(
-                    EBTABLES_CMD + ' -I {{CHAIN_NAME}} {{rule}}')
+                search_cmd_with_mask = EBTABLES_CMD + " -L {{CHAIN_NAME}} | grep -- '{{rule_add}}' > /dev/null"
+                ret = bash_r(search_cmd_with_mask)
+            if ret != 0:
+                add_cmd = EBTABLES_CMD + ' -I {{CHAIN_NAME}} {{rule_add}}'
+                bash_errorout(add_cmd)
 
         def _prepare_dhcp6_iptables(dualStack=True):
             serverip = ip.Ipv6Address(DHCP6_IP)
-            ns_multicast_address = serverip.get_solicited_node_multicast_address() + "/ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff"
-
+            ns_multicast_address = serverip.get_solicited_node_multicast_address()
+            ns_multicast_address_mask = ns_multicast_address + "/ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff"
             if not dualStack:
                 ret = bash_r(EBTABLES_CMD + ' -L {{CHAIN_NAME}} > /dev/null 2>&1')
                 if ret != 0:
@@ -258,16 +265,20 @@ class DhcpEnv(object):
                     bash_errorout(EBTABLES_CMD + ' -A FORWARD -j {{CHAIN_NAME}}')
 
             ns_rule_o = "-p IPv6 -o {{BR_PHY_DEV}} --ip6-dst {{ns_multicast_address}} --ip6-proto ipv6-icmp --ip6-icmp-type neighbour-solicitation -j DROP"
-            _add_ebtables_rule6(ns_rule_o)
+            ns_rule_add = "-p IPv6 -o {{BR_PHY_DEV}} --ip6-dst {{ns_multicast_address_mask}} --ip6-proto ipv6-icmp --ip6-icmp-type neighbour-solicitation -j DROP"
+            _add_ebtables_rule6(ns_rule_o, ns_rule_add)
 
             na_rule_o = "-p IPv6 -o {{BR_PHY_DEV}} --ip6-dst {{ns_multicast_address}} --ip6-proto ipv6-icmp --ip6-icmp-type neighbour-advertisement -j DROP"
-            _add_ebtables_rule6(na_rule_o)
+            ns_rule_add = "-p IPv6 -o {{BR_PHY_DEV}} --ip6-dst {{ns_multicast_address_mask}} --ip6-proto ipv6-icmp --ip6-icmp-type neighbour-advertisement -j DROP"
+            _add_ebtables_rule6(na_rule_o, ns_rule_add)
 
             ns_rule_i = "-p IPv6 -i {{BR_PHY_DEV}} --ip6-dst {{ns_multicast_address}} --ip6-proto ipv6-icmp --ip6-icmp-type neighbour-solicitation -j DROP"
-            _add_ebtables_rule6(ns_rule_i)
+            ns_rule_add = "-p IPv6 -i {{BR_PHY_DEV}} --ip6-dst {{ns_multicast_address_mask}} --ip6-proto ipv6-icmp --ip6-icmp-type neighbour-solicitation -j DROP"
+            _add_ebtables_rule6(ns_rule_i, ns_rule_add)
 
             na_rule_i = "-p IPv6 -i {{BR_PHY_DEV}} --ip6-dst {{ns_multicast_address}} --ip6-proto ipv6-icmp --ip6-icmp-type neighbour-advertisement -j DROP"
-            _add_ebtables_rule6(na_rule_i)
+            ns_rule_add = "-p IPv6 -i {{BR_PHY_DEV}} --ip6-dst {{ns_multicast_address_mask}} --ip6-proto ipv6-icmp --ip6-icmp-type neighbour-advertisement -j DROP"
+            _add_ebtables_rule6(na_rule_i, ns_rule_add)
 
             # prevent ns for dhcp server from upstream network
             dhcpv6_rule_o = "-p IPv6 -o {{BR_PHY_DEV}} --ip6-proto udp --ip6-sport 546:547 -j DROP"
