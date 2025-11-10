32b41b85d8621086f6a3ad1376e253ead8519f36
Zhang Wenhao <wenhao.zhang@zstack.io>
2025-11-10 11:26:02 +0800

    <fix>[zstacklib]: support ky10sp3 2403 os
    This patch is for zsv_4.10.25
    
    Resolves: ZSV-10435
    Related: ZSTAC-70292
    Related: ZSTAC-74658
    
    Change-Id: I77656c7577716e6e766379647971756f71796a6c

--- kvmagent/kvmagent/test/unittest_tools/prepare_env.sh
+++ kvmagent/kvmagent/test/unittest_tools/prepare_env.sh
@@ -7,7 +7,7 @@ set -ex
 # Param version : like 4.6.31
 
 
-YUM_BASED_OS="c76 ky10sp2 ky10sp3"
+YUM_BASED_OS="c76 ky10sp2 ky10sp3 ky10sp3.2403"
 APT_BASED_OS=""
 
 # /root/.zguest/zstack-utility is zstack-utility home of ztest.
@@ -36,6 +36,8 @@ prepare_external_repo() {
             echo later to add
         elif [ "$arch_os" == "aarch64:ky10sp3" ]; then
             echo later to add
+        elif [ "$arch_os" == "aarch64:ky10sp3.2403" ]; then
+            echo later to add # for aarch64:ky10sp3.2403
         fi
         if [ -f "/etc/yum.repos.d/CentOS-Base.repo" ]; then
             sed -i '/mirrors\.aliyuncs\.com/d' /etc/yum.repos.d/CentOS-Base.repo
