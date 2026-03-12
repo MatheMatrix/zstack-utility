55cbeaee30d2f61ce83975a11fb789d10c7cfd45
jin.ma <jin.ma@zstack.io>
2026-03-12 18:47:12 +0800

    <fix>[zstacklib]: upgrade pillow from 2.9.0 to 10.4.0 for Python 3.11 compatibility
    1. Pillow 2.9.0 does not support Python 3.10+, causing console screenshot
       failures after fv-python upgrade to 3.11
    2. Upgrade to Pillow 10.4.0 which supports Python 3.8-3.12
    3. Replace deprecated Image.ANTIALIAS with Image.LANCZOS (removed in 10.0)
    4. Add manylinux_2_28 whl packages for x86_64 and aarch64 architectures
    5. loongarch64 whl needs to be built separately on loongson machine
    
    Resolves: ZSTAC-82671
    
    Change-Id: I7a6f6d636c616a6667716b696c72796661736479

--- zstacklib/zstacklib/utils/image.py
+++ zstacklib/zstacklib/utils/image.py
@@ -4,7 +4,7 @@ import os
 def convert_image(src_path, size=(120, 68), format='PNG'):
     with open(src_path, 'rb') as f:
         img = Image.open(f)
-        img = img.resize(size, Image.ANTIALIAS)
+        img = img.resize(size, Image.LANCZOS)
         img = img.convert('RGB')
     dst_path = os.path.splitext(src_path)[0] + '.' + format.lower()
     with open(dst_path, 'wb') as f:
