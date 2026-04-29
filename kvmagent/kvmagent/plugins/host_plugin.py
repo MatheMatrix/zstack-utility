e7849219259bec554c5f09fb17e083517a7ace52
haoyu.ding <haoyu.ding@zstack.io>
2026-04-29 19:07:32 +0800

    <fix>[host]: Modify RPM database repair
    Resolves: ZSTAC-84832
    
    Change-Id: I75646e61696f76746d6b666273707061636f6668

--- kvmagent/kvmagent/plugins/host_plugin.py
+++ kvmagent/kvmagent/plugins/host_plugin.py
@@ -2029,7 +2029,15 @@ if __name__ == "__main__":
         rsp = UpdateDependencyRsp()
         if self.IS_YUM:
             shell.run(
-                "yum --disablerepo=* --enablerepo=zstack-mn list >/dev/null 2>&1 || (rm -f /var/lib/rpm/__db.*; rpm --rebuilddb)")
+                "timeout --kill-after=10 60 yum --disablerepo=* --enablerepo=zstack-mn list >/dev/null 2>&1 || "
+                "timeout 180 bash -c \""
+                "pkill -9 -f 'yum --disablerepo=\\* --enablerepo=zstack-mn' >/dev/null 2>&1; "
+                "pkill -9 -f 'rpm -qp' >/dev/null 2>&1; "
+                "pkill -9 -f 'rpm --rebuilddb' >/dev/null 2>&1; "
+                "sleep 1; "
+                "rm -f /var/lib/rpm/__db.*; "
+                "rpm --rebuilddb"
+                "\"")
             releasever = kvmagent.get_host_yum_release()
             shell.run("yum remove -y qemu-kvm-tools-ev")
             yum_cmd = "export YUM0={};yum --enablerepo=* clean all && yum --disablerepo=* --enablerepo={} install `cat /var/lib/zstack/dependencies` -y"\
