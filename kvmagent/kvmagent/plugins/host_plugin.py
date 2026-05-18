5d0a862ab54383acb09ef8c7574d790e19f91348
J M <jin.ma@zstack.io>
2026-05-18 13:34:45 +0800

    <fix>[kvm]: report firstConnect and agent start time
    Agent-side companion: HostPlugin captures _agent_start_time_millis at process init and tracks _first_connect_after_boot, both reported on ConnectResponse so MN can drain stale HTTP wrappers that were in flight against the previous agent process. PingResponse unchanged (drain trigger lives only on the connect reply path).
    
    Resolves: ZSTAC-58310
    
    Change-Id: I4adf27200e29766e6fd85a16599c5df8ccb8f885
    (cherry picked from commit b34273c1ea928b40a3aca227ea54ba710f8d620a)

--- kvmagent/kvmagent/plugins/host_plugin.py
+++ kvmagent/kvmagent/plugins/host_plugin.py
@@ -78,6 +78,8 @@ DISTRO_USING_DNF = ['rl84', 'h84r', 'ky10sp1', 'ky10sp2', 'ky10sp3',
 class ConnectResponse(kvmagent.AgentResponse):
     def __init__(self):
         super(ConnectResponse, self).__init__()
+        self.firstConnect = False
+        self.agentStartTimeMillis = 0
 
 class HostCapacityResponse(kvmagent.AgentResponse):
     def __init__(self):
@@ -982,6 +984,8 @@ class HostPlugin(kvmagent.KvmAgent):
         self.IS_YUM = False
         self.IS_APT = False
         self.NVIDIA_SMI_INSTALLED = False
+        self._first_connect_after_boot = True
+        self._agent_start_time_millis = int(time.time() * 1000)
 
         if shell.run("which yum") == 0:
             self.IS_YUM = True
@@ -1025,6 +1029,8 @@ class HostPlugin(kvmagent.KvmAgent):
     def connect(self, req):
         cmd = jsonobject.loads(req[http.REQUEST_BODY])
         rsp = ConnectResponse()
+        rsp.agentStartTimeMillis = self._agent_start_time_millis
+        rsp.firstConnect = self._first_connect_after_boot
 
         # page table extension
         if shell.run('lscpu | grep -q -w GenuineIntel') == 0:
@@ -1082,6 +1088,7 @@ class HostPlugin(kvmagent.KvmAgent):
         bash_r(EBTABLES_CMD + ' -D FORWARD -j ZSTACK-VF-NICS')
         bash_r(EBTABLES_CMD + ' -X ZSTACK-VF-NICS')
 
+        self._first_connect_after_boot = False
         return jsonobject.dumps(rsp)
 
     @thread.AsyncThread
