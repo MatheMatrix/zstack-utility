f6d905d5ae2b8e6c6d63d39bb306f728f4e94e05
shixin.ruan <shixin@shixin.ruan>
2026-05-21 21:44:44 +0900

    <test>[mgt-ipv6]: cover utility dual stack sockets
    Cover utility dual-stack socket helpers used by CherryPy bind
    configuration and port probes.
    
    Change-Id: Ie43cbc2bbe2c5d129088bb26a55dc12a6d64e266
    
    Related: ZSTAC-79206

--- zstacklib/zstacklib/utils/network_ipv6.py
+++ zstacklib/zstacklib/utils/network_ipv6.py
@@ -4,7 +4,9 @@ import socket
 
 
 AGENT_BIND_IP_ENV = 'AGENT_BIND_IP'
+POOLSIZE_ENV = 'POOLSIZE'
 DUAL_STACK_BIND_ADDRESS = '::'
+IPV4_BIND_ADDRESS = ''
 IPV6_SEPARATOR = ':'
 IPV6_BRACKET_PREFIX = '['
 IPV6_BRACKET_SUFFIX = ']'
@@ -14,10 +16,14 @@ LOOPBACK_IPV6 = '::1'
 LINK_LOCAL_IPV6_PREFIX = 'fe80:'
 ZSTACK_RESERVED_INTERFACE_SUFFIX = 'zs'
 IPV6_V6ONLY_DISABLED = 0
+DEFAULT_AGENT_THREAD_POOL = '10'
+CHERRYPY_SOCKET_HOST_KEY = 'server.socket_host'
+CHERRYPY_SOCKET_PORT_KEY = 'server.socket_port'
+CHERRYPY_THREAD_POOL_KEY = 'server.thread_pool'
 
 
 def get_agent_bind_ip(env=None):
-    env = env or os.environ
+    env = os.environ if env is None else env
     return env.get(AGENT_BIND_IP_ENV, DUAL_STACK_BIND_ADDRESS)
 
 
@@ -33,8 +39,30 @@ def get_socket_family(host):
     return socket.AF_INET6 if host and IPV6_SEPARATOR in host else socket.AF_INET
 
 
+def create_tcp_socket_for_host(host):
+    return socket.socket(get_socket_family(host), socket.SOCK_STREAM)
+
+
 def configure_dual_stack_socket(sock):
     sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, IPV6_V6ONLY_DISABLED)
+
+
+def bind_dual_stack_probe_socket(sock, port):
+    configure_dual_stack_socket(sock)
+    sock.bind((DUAL_STACK_BIND_ADDRESS, int(port)))
+
+
+def bind_ipv4_probe_socket(sock, port):
+    sock.bind((IPV4_BIND_ADDRESS, int(port)))
+
+
+def build_cherrypy_socket_config(port, env=None):
+    env = os.environ if env is None else env
+    return {
+        CHERRYPY_SOCKET_HOST_KEY: get_agent_bind_ip(env),
+        CHERRYPY_SOCKET_PORT_KEY: port,
+        CHERRYPY_THREAD_POOL_KEY: int(env.get(POOLSIZE_ENV, DEFAULT_AGENT_THREAD_POOL)),
+    }
 
 
 def is_reportable_agent_address(address, ifname):
