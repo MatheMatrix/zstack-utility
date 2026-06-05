f6487648ecea32b1d7667810d26b79457afa0e96
tao.gan <tao.gan@zstack.io>
2026-06-05 15:41:30 +0800

    <fix>[zstackcli]: zstack-cli support APIPrimaryStorageMigrateVmMsg.volumeMigrationAOs
    Resolves: ZSV-12280
    
    Change-Id: I656b7465726f776c6e78686b6f7568636877676e

--- zstackcli/zstackcli/cli.py
+++ zstackcli/zstackcli/cli.py
@@ -373,6 +373,8 @@ Parse command parameters error:
                     all_params[params[0]] = eval(params[1])
                 elif apiname == 'APICreateVmInstanceMsg' and params[0] == 'diskAOs':
                     all_params[params[0]] = eval(params[1])
+                elif apiname == 'APIPrimaryStorageMigrateVmMsg' and params[0] == 'volumeMigrationAOs':
+                    all_params[params[0]] = eval(params[1])
                 elif apiname in ['APICreateVmInstanceFromTemplatedVmInstanceMsg',
                                  'APICloneVmInstanceMsg'] and params[0] == 'vmCustomSpecification':
                     all_params[params[0]] = eval_string(params[0], params[1])
