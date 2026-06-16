012eb8eab748221cf3fe9a3bb4b66ccc44711c91
shan.wu <shan.wu@zstack.io>
2026-06-16 15:11:45 +0800

    <fix>[sharedblock]: disable lvm dbus notify
    Disable notify_dbus in generated sharedblock LVM configuration to avoid lvmdbusd being triggered for fullreport refreshes.
    
    Resolves: ZSTAC-81484
    
    Change-Id: If630cd59b46f3177e17ae9c5d27023aa4806e809

--- kvmagent/kvmagent/plugins/shared_block_plugin.py
+++ kvmagent/kvmagent/plugins/shared_block_plugin.py
@@ -648,7 +648,8 @@ class SharedBlockPlugin(kvmagent.KvmAgent):
                 "reserved_stack": 256,
                 "reserved_memory": 131072,
                 "use_devicesfile": 0,
-                "use_lvmetad": 1 if enableLvmetad else 0
+                "use_lvmetad": 1 if enableLvmetad else 0,
+                "notify_dbus": 0
             })
             if kvmagent.get_host_os_type() == "debian":
                 config.modify({"udev_rules": 0, "udev_sync": 0})
