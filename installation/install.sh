dfb41243f5d9839bde3ce0bdb94a9899351fa3ad
tao.gan <tao.gan@zstack.io>
2026-08-13 10:14:18 +0800

    <fix>[installation]: enable fstab non-blocking updater for 5.1.1
    Resolves: ZSV-12865
    
    Change-Id: I727a73707871707778696265726364746e787777

--- installation/install.sh
+++ installation/install.sh
@@ -229,6 +229,7 @@ declare -a upgrade_params_array=(
     '4.10.16,-DupgradeFlatDhcpServerIp=true'
     '4.10.16,-DfstabDeviceToUuidUpdater=true'
     '4.10.20,-DupgradeZceXUsernamePasswordMigrate=true'
+    '5.1.1,-DfstabNonBlockingMountOptionsUpdater=true'
 )
 #other than the upon params_array, this one could be persisted in zstack.properties
 declare -a upgrade_persist_params_array=(
